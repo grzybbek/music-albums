b518ca3cb898d1562558e236929fd89938178667
grzybbek <agataewakania@gmail.com>
2020-03-11 08:00:18 +0100

    add new album

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let searchInput = document.getElementById("search");
+var searchInput = document.getElementById("search");
 var albums = document.querySelectorAll('#albums-list li');
 
 searchInput.addEventListener('input', function() {
@@ -13,3 +13,21 @@ searchInput.addEventListener('input', function() {
     });
 });
 
+var addAlbum = document.getElementById("add-album");
+var newAlbum = document.getElementById("new-album");
+
+addAlbum.addEventListener('click', function() {
+    let ulAlbums = document.getElementById("albums-list");
+    let liAlbum = document.createElement("li");
+
+    liAlbum.appendChild(document.createTextNode(newAlbum.value));
+    liAlbum.setAttribute('class', 'list-group-item');
+    ulAlbums.appendChild(liAlbum);
+});
+
+newAlbum.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addAlbum.click();
+    }
+});
