a77468098f87897d8c48d64d78a134b8d9f5ae2e
grzybbek <agataewakania@gmail.com>
2020-03-11 14:09:51 +0100

    list sorting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var albums = document.querySelectorAll('#albums-list li');
 searchInput.addEventListener('input', function() {
     let searchPhrase = searchInput.value.toUpperCase();
 
-    albums.forEach(function (item, index) {
+    albums.forEach(function (item) {
         if (searchPhrase === '' || item.innerHTML.toUpperCase().includes(searchPhrase)) {
             item.removeAttribute('hidden');
         } else {
@@ -15,9 +15,9 @@ searchInput.addEventListener('input', function() {
 
 var addAlbum = document.getElementById("add-album");
 var newAlbum = document.getElementById("new-album");
+var ulAlbums = document.getElementById("albums-list");
 
 addAlbum.addEventListener('click', function() {
-    let ulAlbums = document.getElementById("albums-list");
     let liAlbum = document.createElement("li");
 
     liAlbum.appendChild(document.createTextNode(newAlbum.value));
@@ -31,3 +31,31 @@ newAlbum.addEventListener('keyup', function(event) {
         addAlbum.click();
     }
 });
+
+var azSort = document.getElementById("azSort");
+var zaSort = document.getElementById("zaSort");
+
+azSort.addEventListener('click', () => { sortAlbums(true) });
+zaSort.addEventListener('click', () => { sortAlbums(false) });
+
+function sortAlbums(ascending) {
+    let albumsArray = [];
+    ulAlbums.childNodes.forEach((item) => {
+        if (item.nodeName === 'LI') {
+            albumsArray.push(item);
+        }
+    });
+    albumsArray.sort((item1, item2) => {
+        let text1 = item1.innerHTML.toUpperCase();
+        let text2 = item2.innerHTML.toUpperCase();
+        
+        if (ascending) {
+            return (text1 < text2) ? -1 : (text1 > text2) ? 1 : 0;
+        } else {
+            return (text1 > text2) ? -1 : (text1 < text2) ? 1 : 0;
+        }
+    });
+    albumsArray.forEach((item) => {
+        ulAlbums.appendChild(item);
+    })
+}
